f18119b437933a6dab3b21bf45bdfd1377c9e0b8
张秀雨 <1292460369@qq.com>
2020-12-23 17:31:29 +0800

    新增jwt扩展信息

--- src/main/java/blog/service/UserSession.java
+++ src/main/java/blog/service/UserSession.java
@@ -2,6 +2,8 @@ package blog.service;
 
 import blog.mapper.UserMapper;
 import blog.model.UserModel;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -20,14 +22,18 @@ public class UserSession {
     UserMapper userMapper;
 
     public void addUserSession(String openid,HttpServletRequest request) {
-
+        Jws<Claims> jws;
         if (openid != null){
             List<UserModel> userModels = userMapper.OneOpenid(openid);
             Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-            String jws = Jwts.builder().setSubject(userModels.toString()).signWith(key).compact();
+            String jwss = Jwts.builder().setSubject(userModels.toString()).signWith(key).compact();
+            jws = Jwts.parserBuilder()  // (1)
+                    .setSigningKey(key)         // (2)
+                    .build()                    // (3)
+                    .parseClaimsJws(jwss); // (4)
 
-
+            System.out.println(jws);
         }
     }
 
